80226a08bbcc535d05dc40eff7f8d676ee63dd89
hadinurhakim <61036860+hadinurhakim@users.noreply.github.com>
2020-07-23 16:52:54 +0700

    Major-Steps

--- src/App.js
+++ src/App.js
@@ -3,9 +3,60 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <div className="header">
+          <button className="header-btn main-black-color">Add</button>
+          <h1 className="header-title">Todo List</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </div>
+
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn">Add</button>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">wwkwkw land</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
